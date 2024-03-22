8e9abb48009eb6e562bb64927ccc44ebd52bce19
NikolozR <nika.rusishvili.95@gmail.com>
2024-03-22 19:16:10 +0400

    Contact page done

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import Button from "./Button";
+import '../styles/Contact.scss'
 
 function Contact() {
   return (
-    <div>Contact</div>
-  )
+    <section className="contact">
+      <div className="container">
+        <h1 className="contact-header">Contact Us</h1>
+        <div className="contact-body">
+          <form action="#" method="post">
+            <input placeholder="Full Name" type="text" name="fullName" id="fullName" />
+            <input placeholder="E-mail" type="email" name="email" id="email" />
+            <input placeholder="Message" type="text" name="message" id="message" />
+            <Button>Contact us</Button>
+          </form>
+          <div className="contact-info">
+            <h2>Contact</h2>
+            <p className="info contact-mail">BUDU@BUDUUU.com</p>
+            <h2>Based in</h2>
+            <p className="info contact-place">Kutaisi, Georgia</p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
 
-export default Contact
+export default Contact;
