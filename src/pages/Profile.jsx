5f16fbda0a3dbc48a36ac6d2cf2359a8d3d6e905
NikolozR <nika.rusishvili.95@gmail.com>
2024-03-30 15:15:45 +0400

    password with states done

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,22 +1,47 @@
 import { profileData } from "../data/profileData";
 import Button from "../components/Button";
-import '../styles/Profile.scss'
+import "../styles/Profile.scss";
+import { useState } from "react";
 
 function Profile() {
-  return <section className="profile">
-    <div class="container">
+  const [pasword, setPassword] = useState("");
+  const [confirmation, setConfirmation] = useState("");
+
+  const handleChange = (e, type) => {
+    if (type === "new") setPassword(e.target.value);
+    else if (type === "confirm") setConfirmation(e.target.value);
+  };
+
+  return (
+    <section className="profile">
+      <div class="container">
         <h1>User Profile</h1>
         <p className="full-name">Full Name: {profileData.fullName}</p>
         <p className="surname">Surname: {profileData.surname}</p>
         <p className="email">Email: {profileData.email}</p>
         <p class="bio">Bio: {profileData.bio}</p>
         <form action="" method="post">
-            <input type="password" placeholder="New Password" name="newPassword" id="newPassword" />
-            <input type="password" placeholder="Confirm New Password" name="confirmPassword" id="confirmPassword" />
-            <Button>Save</Button>
+          <input
+            type="password"
+            value={pasword}
+            onChange={(e) => handleChange(e, "new")}
+            placeholder="New Password"
+            name="newPassword"
+            id="newPassword"
+          />
+          <input
+            type="password"
+            value={confirmation}
+            onChange={(e) => handleChange(e, "confirm")}
+            placeholder="Confirm New Password"
+            name="confirmPassword"
+            id="confirmPassword"
+          />
+          <Button>Save</Button>
         </form>
-    </div>
-  </section>;
+      </div>
+    </section>
+  );
 }
 
 export default Profile;
