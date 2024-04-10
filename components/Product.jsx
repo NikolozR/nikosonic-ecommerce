d49d0d4ed504a7ba8c037e98a6f4d9d13e58a707
NikolozR <nika.rusishvili.95@gmail.com>
2024-04-11 01:56:02 +0400

    added product pages

--- components/Product.jsx
+++ components/Product.jsx
@@ -6,27 +6,33 @@ function Product({ prodData }) {
   return (
     <div className="min-w-[300px] max-w-[300px] bg-customMain rounded-[20px] pt-[17px] pr-[15px] pb-[10px] pl-[15px]">
       <Image
-          src={prodData.images[0]}
-          priority={true}
-          width={500}
-          height={500}
-          className="rounded-[10px] object-contain w-[270px] h-[176px]"
-          alt="Product Image"
-        />
-        <div className="pt-[15px]">
-          <h4 className="text-white text-[25px] font-bold mb-[15px]">
-            {prodData.title}
-          </h4>
-          <p className="text-white text-[14px] font-light leading-[29px] mb-[20px]">
-            {prodData.description}
+        src={prodData.images[0]}
+        priority={true}
+        width={500}
+        height={500}
+        className="rounded-[10px] object-contain w-[270px] h-[176px]"
+        alt="Product Image"
+      />
+      <div className="pt-[15px]">
+        <h4 className="text-white text-[21px] font-bold mb-[15px]">
+          {prodData.title}
+        </h4>
+        <p className="text-white text-[14px] font-light leading-[29px] mb-[20px]">
+          {prodData.description}
+        </p>
+        <div className="flex items-center justify-between">
+          <p className="text-[27px] text-white font-extrabold">
+            ${prodData.price}0
           </p>
-          <div className="flex items-center justify-between">
-            <p className="text-[27px] text-white font-extrabold">
-              ${prodData.price}0
-            </p>
-            <Button>Add to cart</Button>
-          </div>
         </div>
+        <div className="flex justify-between mt-[20px]">
+          <Button>Add to cart</Button>
+        <Link href={'/products/' + prodData?.id}>
+          <Button>Read More</Button>
+        </Link>
+
+        </div>
+      </div>
     </div>
   );
 }
