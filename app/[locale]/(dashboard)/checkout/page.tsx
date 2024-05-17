929c84f4f8dba1734a034609c35959b213642917
TeaChareqishvili <teachareqishvili89@gmail.com>
2024-05-18 00:56:11 +0400

    render products

--- app/[locale]/(dashboard)/checkout/page.tsx
+++ app/[locale]/(dashboard)/checkout/page.tsx
@@ -1,12 +1,17 @@
 import { getCart } from "../../../api/api";
+import Image from "next/image";
 
 export default async function CheckOut() {
   const cart = await getCart("29");
   const cartItems = await cart.json();
   console.log(cartItems, "items");
 
-  // Fetch product details for each productid in the cart
-  const productPromises = cartItems.rows.map((item) =>
+  const productQuantityMap = [];
+  cartItems.rows.forEach((item) => {
+    productQuantityMap[item.productid] = item.quantity;
+  });
+
+  const productPromises = cartItems.rows.map((item: Product) =>
     fetch(`https://dummyjson.com/products/${item.productid}`).then((res) =>
       res.json()
     )
@@ -14,19 +19,27 @@ export default async function CheckOut() {
 
   const products = await Promise.all(productPromises);
 
-  console.log(products, "products");
+  //   console.log(products, "products");
 
   return (
     <div className="w-4/5 mx-auto mt-[30px]">
       <h1>This is the checkout page</h1>
       <div>
-        {/* {products?.map((product) => (
+        {products.map((product) => (
           <div key={product.id}>
+            <Image
+              width={100}
+              height={100}
+              src={product.thumbnail}
+              alt="prodcut"
+            />
             <h2>{product.title}</h2>
+            <p>{product.brand}</p>
             <p>{product.description}</p>
             <p>Price: ${product.price}</p>
+            <p>Quantity: {productQuantityMap[product.id]}</p>
           </div>
-        ))} */}
+        ))}
       </div>
     </div>
   );
