42fbdab7a6d08ea44a71d7f529a5470aee029d49
NikolozR <nika.rusishvili.95@gmail.com>
2024-06-12 04:28:22 +0400

    build error fix

--- app/[locale]/(dashboard)/products/[productId]/page.tsx
+++ app/[locale]/(dashboard)/products/[productId]/page.tsx
@@ -10,8 +10,8 @@ import { getAuth0User } from "../../../../actions";
 
 export async function generateStaticParams() {
   const idsRes: { product_id: number }[] = await getProductIds();
-  const ids = idsRes.map((el) => el.product_id + '');
-  return ids.map((id) => ({
+  const ids = idsRes?.map((el) => el.product_id + '');
+  return ids?.map((id) => ({
     productId: id,
   }));
 }
